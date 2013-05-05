3711d95b8ac3f50af067301a4d326becb2a88e99
Jason Teng <kimi00jason@gmail.com>
2013-05-04 23:34:20 -0700

    Revert "Flood fill finished.    Hamster Version: 1.3"
    This reverts commit 85f80ccb46857ab4205140ef93fbc4088c8cbd94.

--- main/maze.h
+++ main/maze.h
@@ -10,12 +10,7 @@ public:
   void mapping();
   void adjacentWall(Cell*);
   
-  void flood();
-  void expand(int, int, int);
-  void northFlood(int, int, int);
-  void eastFlood(int, int, int);
-  void southFlood(int, int, int);
-  void westFlood(int, int, int);
+  void floodFill();
   
   void initialize();
 };
